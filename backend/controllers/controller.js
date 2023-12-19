d391d4f05c109d0b07bd1437310845464ec6e99b
Rushabh Trivedi <41021094+rushabhT3@users.noreply.github.com>
2023-12-19 10:18:15 +0530

    retrieving mail from backend for user

--- backend/controllers/controller.js
+++ backend/controllers/controller.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const User = require("../models/model");
-const Mail = require('../models/mail');
+const Mail = require("../models/mail");
 
 const helloWorld = (req, res) => {
   res.send("Hello, World!");
@@ -43,25 +43,51 @@ const compose = async (req, res) => {
     const { sender, receiver, subject, content } = req.body;
     if (!sender || !receiver || !subject || !content) {
       return res.status(400).json({
-        message: 'Sender, receiver, subject, and content are required',
+        message: "Sender, receiver, subject, and content are required",
       });
     }
     const mail = await Mail.create({
       sender,
       receiver,
       subject,
-      content
+      content,
     });
     res.status(201).json({
-      message: 'Mail successfully sent',
-      mail: mail
+      message: "Mail successfully sent",
+      mail: mail,
     });
   } catch (error) {
     res.status(500).json({
-      message: 'An error occurred while sending the mail',
-      error: error.message
+      message: "An error occurred while sending the mail",
+      error: error.message,
     });
   }
 };
 
-module.exports = { signUp, helloWorld, login, compose };
+const findMailBySender = async (req, res) => {
+  try {
+    const { sender } = req.query;
+    if (!sender) {
+      return res.status(400).json({
+        message: "Sender is required",
+      });
+    }
+    const mails = await Mail.findAll({
+      where: {
+        receiver: sender,
+      },
+    });
+
+    res.status(200).json({
+      message: "Emails found by sender successfully",
+      emails: mails,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "An error occurred while finding emails by sender",
+      error: error.message,
+    });
+  }
+};
+
+module.exports = { signUp, helloWorld, login, compose, findMailBySender };
--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { helloWorld, signUp, login, compose } = require("../controllers/controller");
+const { helloWorld, signUp, login, compose, findMailBySender } = require("../controllers/controller");
 
 // define the home page route
 router.get("/", helloWorld);
@@ -11,5 +11,7 @@ router.post("/signUp", signUp);
 router.post("/login", login);
 
 router.post('/compose', compose)
+
+router.get('/findMails', findMailBySender)
 
 module.exports = router;
